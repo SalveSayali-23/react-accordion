fc8d147deed503e3910b6a28843938c43df03ee2
Sayali <salvesayali23@gmail.com>
2024-07-23 23:40:28 +0530

    changes done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Accordion from './components/accordion'
+
+const App = () => {
+  return (
+    <div>
+      {/* Accordion componnet */}
+      <Accordion/>
+    </div>
+  )
+}
+
+export default App
